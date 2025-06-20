78db0867b95f19c90024f72841e9976153604653
Suraj Kumar <suraj06022003@gmail.com>
2025-06-20 22:13:49 +0530

    feat: implement counter functionality with increment, decrement, and reset features

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+
+  const[count, setCount] = useState(0);
+
+  function decreaseHandler() {
+    // Logic to decrease the counter
+    setCount(count - 1);
+  }
+
+  function increaseHandler() {
+    // Logic to increase the counter
+    setCount(count + 1);
+  }
+  function resetHandler() {
+    // Logic to reset the counter
+    setCount(0);
+  }
+
   return (
-    <main className="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 className="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p className="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" className="mx-auto" />
-    </main>
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+      <div className="text-[#0398d4] font-medium text-">
+        Increment & Decrement
+      </div>
+      <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+        <button onClick={decreaseHandler} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">
+          -
+        </button>
+        <div className="font-bold gap-12 text-5xl">
+          {count}
+        </div>
+        <button onClick={increaseHandler} className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl">
+          +
+        </button>
+      </div>
+      <button onClick={resetHandler} className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg">
+        Reset
+      </button>
+    </div>
   );
 }
 
